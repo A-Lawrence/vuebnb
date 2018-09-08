a1f86bb969ee942df1e024afd87962135d25eba7
Anthony Lawrence <artisan@anthonylawrence.me.uk>
2018-09-08 22:48:16 +0100

    Completed chapter 5 of the booook.

--- vuebnb/app/Http/Controllers/ListingController.php
+++ vuebnb/app/Http/Controllers/ListingController.php
@@ -9,13 +9,23 @@ class ListingController extends Controller
 {
     public function get_listing_api(Listing $listing)
     {
+        return response()->json($this->get_listing($listing));
+    }
+
+    public function get_listing_web(Listing $listing)
+    {
+        return view('app')->with('model', $this->get_listing($listing));
+    }
+
+    protected function get_listing(Listing $listing)
+    {
         $model = $listing->toArray();
 
-        for($i=1; $i<=4; $i++){
-            $filepath = sprintf("images/%d/Image_%d.jpg", $model['id'], $i);
+        for ($i = 1; $i <= 4; $i++) {
+            $filepath             = sprintf("images/%d/Image_%d.jpg", $model['id'], $i);
             $model['image_' . $i] = asset($filepath);
         }
 
-        return response()->json($model);
+        return $model;
     }
 }
