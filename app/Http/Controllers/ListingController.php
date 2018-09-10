63779a7a6f6020c89b9f5e7cbc359ce1b33ead13
Anthony Lawrence <artisan@anthonylawrence.me.uk>
2018-09-10 21:03:41 +0100

    Part way through chp7.

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -7,6 +7,26 @@ use Illuminate\Http\Request;
 
 class ListingController extends Controller
 {
+    public function get_homepage()
+    {
+        return view('app', [
+            'data' => collect([
+                'listings' => Listing::all([
+                    'id',
+                    'address',
+                    'title',
+                    'price_per_night',
+                ])->map(function ($listing) {
+                    $filepath = sprintf("images/%d/Image_1_thumb.jpg", $listing->id);
+                    
+                    $listing->thumb = asset($filepath);
+
+                    return $listing;
+                })->toArray(),
+            ]),
+        ]);
+    }
+
     public function get_listing_api(Listing $listing)
     {
         return response()->json($this->get_listing($listing));
@@ -14,7 +34,7 @@ class ListingController extends Controller
 
     public function get_listing_web(Listing $listing)
     {
-        return view('app')->with('model', $this->get_listing($listing));
+        return view('app')->with('data', $this->get_listing($listing));
     }
 
     protected function get_listing(Listing $listing)
@@ -26,6 +46,6 @@ class ListingController extends Controller
             $model['image_' . $i] = asset($filepath);
         }
 
-        return $model;
+        return collect(['listing' => $model]);
     }
 }
