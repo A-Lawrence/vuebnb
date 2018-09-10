e9475e2238d0ec39d34254a3f8f4af0ed0320980
Anthony Lawrence <artisan@anthonylawrence.me.uk>
2018-09-10 20:20:09 +0100

    Completed chp7.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,37 +1,11 @@
 import "core-js/fn/object/assign";
 import Vue from 'vue';
-import {populateAmenitiesAndPrices} from './helpers';
 
-let model = JSON.parse(window.vuebnb_listing_model);
-model = populateAmenitiesAndPrices(model);
-
-import HeaderImage from './components/HeaderImage.vue';
-import ModalWindow from './components/ModalWindow.vue';
-import ImageCarousel from './components/ImageCarousel.vue';
-import FeatureList from './components/FeatureList.vue';
+import ListingPage from './components/ListingPage.vue';
 
 var app = new Vue({
     el: '#app',
-    data: Object.assign(model, {
-        headerImageStyle: {
-            'background-image': `url(${model.images[0]})`,
-        },
-        contracted: true,
-    }),
-    methods: {
-        openModal() {
-            this.$refs.imagemodal.modalOpen = true;
-        },
-    },
-    components: {
-        HeaderImage,
-        ModalWindow,
-        ImageCarousel,
-        FeatureList,
-    },
-    destroyed: function () {
-        document.removeEventListener('keyup', this.escapeKeyListener);
-    },
+    render: h => h(ListingPage)
 });
 
 document.addEventListener('keyup', function (evt) {
