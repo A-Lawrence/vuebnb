0383784f62a4d70e4703594f5d87bd0b7b1e9872
Anthony Lawrence <artisan@anthonylawrence.me.uk>
2018-09-11 18:36:22 +0100

    Saved/Hearted properties stored in Vuex.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,17 +1,13 @@
 import "core-js/fn/object/assign";
 import Vue from 'vue';
 
+import router from './router';
+import store from './store';
 import App from './components/App.vue';
-import router from './router'
 
 var app = new Vue({
     el: '#app',
     render: h => h(App),
     router,
-});
-
-document.addEventListener('keyup', function (evt) {
-    if (evt.keyCode === 27 && app.modalOpen) {
-        app.modalOpen = false;
-    }
+    store
 });
